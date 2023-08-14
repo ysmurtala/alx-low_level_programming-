d12150f5cb6c84ffbc926d93e7f1fb3ab6c48810
ysmurtala <yaqoobmurry@gmail.com>
2023-08-13 20:50:43 -0700

    Smile in the mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - this is the main
+ * Return: always 0
+ */
+
+int main(void)
+{
+	char i;
+
+	for (i = 'z' ; i > 'z' ; i--)
+	putchar(i);
+	putchar('\n');
+	return (0);
+}
