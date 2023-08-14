848c678d1b3d76e382b7d06113bf463cea4225fb
ysmurtala <yaqoobmurry@gmail.com>
2023-08-14 08:35:36 -0700

    Size is not grandeur, and territory does not make a nation

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,37 +1,20 @@
 #include<stdio.h>
 /**
-* main - Entry point
+* main - main function
 *
-* Return: Always 0 (Success)
+* Return: always 0
 */
 int main(void)
 {
-	printf("Size of a char: %d byte(s)\n", sizeof(char));
-	printf("Size of an int: %d byte(s)\n", sizeof(int));
-	printf("Size of a long int: %d byte(s)\n", sizeof(long int));
-	printf("Size of a long long int: %d byte(s)\n", sizeof(long long int));
-	printf("Size of a float: %d byte(s)\n", sizeof(float));
+	printf("Size of char: %d bytes\n", sizeof(char));
+	printf("Size of short: %d bytes\n", sizeof(short));
+	printf("Size of int: %d bytes\n", sizeof(int));
+	printf("Size of long: %d bytes\n", sizeof(long));
+	printf("Size of long long: %d bytes\n", sizeof(long long));
+	printf("Size of float: %d bytes\n", sizeof(float));
+	printf("Size of double: %d bytes\n", sizeof(double));
+	printf("Size of long double: %d bytes\n", sizeof(long double));
+
+
 	return (0);
-}
-
-100-intel
-
-#!/bin/bash
-gcc -S -masm=intel $CFILE
-
-101-quote.c
-
-#include <stdio.h>
-#include <unistd.h>
-
-/**
-* main - Entry point
-*
-* Return: Always 1 (Success)
-*/
-int main(void)
-{
-	write(2,
-	"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-	return (1);
 }
