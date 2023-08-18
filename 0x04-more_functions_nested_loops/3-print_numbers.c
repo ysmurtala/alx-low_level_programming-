ea5f6403ebb5d852510be65ac7639cd49e2596dc
ysmurtala <yaqoobmurry@gmail.com>
2023-08-18 09:01:58 -0700

    The numbers speak for themselves

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+#ifndef PUTCHAR_H
+#define PUTCHAR_H
+
+int _putchar(char c);
+
+#endif /* PUTCHAR_H */
+
+/**
+ * print_numbers - print numbers from 0 to 9
+ * Return: void
+ */
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0 ; i < 10 ; i++)
+	{
+		_putchar(i + '0');
+	}
+	_putchar('\n');
+}
